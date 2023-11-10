2c896448952f56caffd377d31f6bf919f9e994ec
doug <douglas.lusan@gmail.com>
2023-11-10 13:25:55 -0300

    primeiro commit

---
+++ ScreenSound/Modelos/Avaliacao.cs
@@ -0,0 +1,18 @@
+﻿
+namespace ScreenSound.Modelos;
+
+internal class Avaliacao
+{
+    public int Nota { get; set; }
+
+    public Avaliacao(int nota)
+    {
+        Nota = nota;
+    }
+
+    public static Avaliacao Parse(string texto)
+    {
+        int nota = int.Parse(texto);
+        return new Avaliacao(nota);
+    }
+}
